df13a36626219895e164ca99030cd10cda31d82b
Jonathan Britton <jbritttt29@hotmail.com>
2023-01-20 06:48:18 +0000

    todo list created

---
+++ assets/list-of-todos.js
@@ -0,0 +1,32 @@
+// add some more e-golf info below the second slider
+
+// finish contact form layout and team image. add some text for this image encourage the user to register an interest.
+
+// fix hero image on mobile position overlay text better
+
+// fix spacing between elements on mobile
+
+// create icons for the slider buttons
+
+// style mobile nav menu a bit better. ***Remember it is the same nav used on mobile as desktop***
+
+// validate code
+
+// improve lighthouse scores as much as possible needs some miner changes
+
+
+// make a favicon
+
+
+
+
+
+// if there is time.. use jquery for smooth scroll from every enquire now button to the form at bottom of page. If there is no time then just use scroll behaviour smooth with css.
+
+
+
+// if there is time go over the code and write down the technique that I used to position elements in each section just incase im asked to make changes on the spot.
+
+// if there is time change pixels to rems
+
+// if there is time.. validate form and create fetch api and set it up for a post request 
