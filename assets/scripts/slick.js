ed9a26380136d0ff18d0f77fb733d8cfec7e386d
Jonathan Britton <jbritttt29@hotmail.com>
2023-01-21 00:05:21 +0000

    format code

--- assets/scripts/slick.js
+++ assets/scripts/slick.js
@@ -1,61 +1,55 @@
-
-
 // first slider
 $(".slick-slider").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
 
-    prevArrow: ".prev",
-    nextArrow: ".next",
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 1180,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
+  prevArrow: ".prev",
+  nextArrow: ".next",
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1180,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
       },
+    },
 
-      {
-        breakpoint: 830,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
+    {
+      breakpoint: 830,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
-     
-    ],
-  });
+    },
+  ],
+});
 
+// second slider. The only difference is the width on images is set to 70% and it doesnt have auto play.
+$(".slick-slider-1").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
 
-  // second slider. The only difference is the width on images is set to 70% and it doesnt have auto play.
-  $(".slick-slider-1").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  
-
-    prevArrow: ".prev",
-    nextArrow: ".next",
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
+  prevArrow: ".prev",
+  nextArrow: ".next",
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
       },
+    },
 
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
-    ],
-  });
-
+    },
+  ],
+});
