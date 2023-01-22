44299fd4b1cb8b94bab9c707772a8f9f83ae49b2
Jonathan Britton <jbritttt29@hotmail.com>
2023-01-22 10:54:37 +0000

    new todo list created

---
+++ assets/new-to-do.js
@@ -0,0 +1,30 @@
+
+
+// create drop down menu on hover from items in header. Place some arrow icons below listitems.
+
+// create dropdown search bar in header jquery or vanilla.
+
+// find an api online and display dynamic data. ideally using user input for the request.
+
+// fix page overflow on mobile. possible padding/margin issue before observer fires.
+
+// create read more functionality. maybe in our team section before footer.
+
+// use observer on icons before footer. Slide up or fade in.
+
+// post form to dummy api display and thankyou message only if request is a success.
+
+// set width of image sliders to 100% on mobile and ajust arrow icons
+
+// hide the nav menu on mobile when the user clicks on a link
+
+// position and resize (class="icons-container") for tablet screen. And center it on mobile. Might need to rebuild it.
+
+// Drink loads of coke and eat loads of pizzas
+
+
+
+
+
+
+
