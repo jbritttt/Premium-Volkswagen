7832ceb32ac6feb186f5f9910ff779d644414483
Jonathan Britton <jbritttt29@hotmail.com>
2023-01-26 17:44:53 +0000

    todo list updated

--- assets/new-to-do.js
+++ assets/new-to-do.js
@@ -12,7 +12,19 @@
 
  hide the nav menu on mobile when the user clicks on a link
 
-Drink loads of coke and eat loads of pizzas
+ improve the postion of font awsome search icon on header for mobiles. It is positioned too far to the left.
+
+ create read more functionality. maybe in our team section before footer.
+
+ use observer on icons before footer. Slide up or fade in.
+
+ post form to dummy api display and thankyou message only if request is a success.
+
+ ajust scroll position when user scrolls to form on mobile screens. The highlighted yellow text must be visable.
+
+ position and resize (class="icons-container") for tablet screen. And center it on mobile. Might need to rebuild it.
+ 
+ Drink loads of coke and eat loads of pizzas
 
 */
 
@@ -21,17 +33,7 @@ Drink loads of coke and eat loads of pizzas
 
 // ToDO List
 
-// improve the postion of font awsome search icon on header for mobiles. It is positioned too far to the left.
-
-// create read more functionality. maybe in our team section before footer.
-
-// use observer on icons before footer. Slide up or fade in.
-
-// post form to dummy api display and thankyou message only if request is a success.
-
-// ajust scroll position when user scrolls to form on mobile screens. The highlighted yellow text must be visable.
-
-// position and resize (class="icons-container") for tablet screen. And center it on mobile. Might need to rebuild it.
+// Nothing left todo I am finished 
 
 
 
